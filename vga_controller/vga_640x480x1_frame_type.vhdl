60dc369d52197e18fecd1de1e05dd7e24c9c9cf8
MarcelMG <28888720+MarcelMG@users.noreply.github.com>
2021-01-15 11:28:07 +0100

    Delete vga_controller directory

--- vga_controller/vga_640x480x1_frame_type.vhdl
+++
@@ -1,9 +0,0 @@
-library ieee;
-use ieee.std_logic_1164.all;
-
-package vga_640x480x1_frame_type is
-        type vga_640x480x1_frame is array(0 to 640-1) of std_logic_vector(0 to 480-1);
-end package;
-
-package body vga_640x480x1_frame_type is
-end package body vga_640x480x1_frame_type;
